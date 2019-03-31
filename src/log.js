dd0eba638a7e4ef7e78662321f3dbd9f80de0821
Isha Gautam <ishagautam1998@gmail.com>
2019-03-31 21:25:48 +0530

    Update log.js

--- src/log.js
+++ src/log.js
@@ -2,7 +2,7 @@ var winston = require('winston');
 require('winston-daily-rotate-file');
 
 var transport = new (winston.transports.DailyRotateFile)({
-    filename: 'C:/Users/ISHA/Desktop/REPOS/AnthroLink/logs/AnthroLink-%DATE%.log',
+    filename: '../logs/AnthroLink-%DATE%.log',
     datePattern: 'YYY-MM-DD-HH',
     maxSize: '20m',
     maxFiles: '30d'
