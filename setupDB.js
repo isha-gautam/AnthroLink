106bf50787c842a924cfe7515a965f7cb5310457
Isha Gautam <igautam24@yahoo.com>
2019-04-04 20:58:10 +0530

    Setup mongoDB and created storage folder (#8)
    * Setup mongoDB and created storage folder
    
    * Made changes in setup files

---
+++ setupDB.js
@@ -0,0 +1,26 @@
+// JavaScript source code
+conn = new Mongo();
+db = conn.getDB("AnthroLinkDB");
+
+db.createUser(
+    {
+        user: "AnthroAdmin",
+        pwd: "password",
+
+        roles: [{ role: "userAdmin", db: "AnthroLinkDB" }]
+    }
+);
+
+db.createCollection("users");
+db.createCollection("tickets");
+
+db = conn.getDB("admin");
+
+db.createUser(
+    {
+        user: "adminDB",
+        pwd: "password",
+
+        roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
+    }
+);
